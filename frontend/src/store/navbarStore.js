e18ffddcb3d6e5f22f36932018cba6c0842c7b8c
RogerGTWARR <rperezcastillo912@gmail.com>
2025-10-23 00:51:53 -0600

    ..

--- frontend/src/store/navbarStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-export const useNavbarStore = create((set) => ({
-    isMenuOpen: false,
-    toggleMenu: () => set((state) => ({ isMenuOpen: !state.isMenuOpen })),
-    closeMenu: () => set({ isMenuOpen: false }),
-    setMenuOpen: (open) => set({ isMenuOpen: open })
-}));
